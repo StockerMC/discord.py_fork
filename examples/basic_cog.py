8004d332cd323a50c1bffc2963eb0cb295f1f03f
StockerMC <44980366+StockerMC@users.noreply.github.com>
2021-09-23 20:07:16 -0400

    Add basic cog example

---
+++ examples/basic_cog.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.members = True
+        super().__init__(command_prefix=commands.when_mentioned_or('$'), intents=intents)
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+class Greetings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        channel = member.guild.system_channel
+        if channel is not None:
+            await channel.send(f'Welcome {member.mention}.')
+
+    @commands.command()
+    async def hello(self, ctx: commands.Context, *, member: discord.Member = None):
+        """Says hello"""
+        member = member or ctx.author
+        if self._last_member is None or self._last_member != member:
+            await ctx.send(f'Hello {member.name}~')
+        else:
+            await ctx.send(f'Hello {member.name}... This feels familiar.')
+        self._last_member = member
+
+bot = MyBot()
+bot.add_cog(Greetings(bot))
+bot.run('token')
