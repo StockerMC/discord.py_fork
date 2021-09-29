8bfbf4cea8d5ab26a7dd3b308525af0fe410bc3d
StockerMC <44980366+StockerMC@users.noreply.github.com>
2021-09-28 21:08:30 -0400

    Use a default in slash command example

--- examples/application_commands/slash_commands.py
+++ examples/application_commands/slash_commands.py
@@ -9,6 +9,10 @@ class MyClient(discord.Client):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
+class AuthorDefault(discord.ApplicationCommandOptionDefault):
+    async def default(self, response: discord.SlashCommandResponse):
+        return response.user
+
 # setting `guild_ids` in development is better when possible because
 # registering global commands has a 1 hour delay
 class Avatar(discord.SlashCommand, guild_ids=[123]):
@@ -16,7 +20,7 @@ class Avatar(discord.SlashCommand, guild_ids=[123]):
 
     # the `required` kwarg keyword argument can also be set to `False`
     # instead of typehinting the argument as optional
-    user: typing.Optional[discord.User] = discord.application_command_option(description='The user to get the avatar from')
+    user: typing.Optional[discord.User] = discord.application_command_option(description='The user to get the avatar from', default=AuthorDefault)
 
     async def callback(self, response: discord.SlashCommandResponse):
         avatar = response.options.user.display_avatar.url
