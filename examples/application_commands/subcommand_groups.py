383f740a3506ded177b15c4e623f2862efbc0841
StockerMC <44980366+StockerMC@users.noreply.github.com>
2021-10-02 10:27:36 -0400

    Update application command subcommand group example to use defaults

--- examples/application_commands/subcommand_groups.py
+++ examples/application_commands/subcommand_groups.py
@@ -9,6 +9,10 @@ class MyClient(discord.Client):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
+class AuthorDefault(discord.ApplicationCommandOptionDefault):
+    async def default(self, response: discord.SlashCommandResponse):
+        return response.user
+
 # setting `guild_ids` in development is better when possible because
 # registering global commands has a 1 hour delay
 class Permissions(discord.SlashCommand, guild_ids=[123]):
@@ -20,13 +24,10 @@ class User(discord.SlashCommand, parent=Permissions, group=True):
 class Get(discord.SlashCommand, parent=User):
     """Get permissions for a user in the specified or current channel"""
 
-    user: typing.Optional[discord.User] = discord.application_command_option(description='The user to get')
+    user: typing.Optional[discord.User] = discord.application_command_option(description='The user to get', default=AuthorDefault)
 
     async def callback(self, response: discord.SlashCommandResponse):
         user = response.options.user
-        if user is None:
-            user = response.user
-
         embed = discord.Embed(title=f'Permissions for {user}')
         for permission, value in response.channel.permissions_for(user):
             embed.add_field(name=permission.title().replace('_', ' '), value=value)
