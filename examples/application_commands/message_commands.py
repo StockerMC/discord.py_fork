45aa4c2f72629dba089d274da2ba17a170b9eb28
Stocker <44980366+StockerMC@users.noreply.github.com>
2021-10-12 10:27:23 -0400

    Remove unnecessary __init__ from message command example

--- examples/application_commands/message_commands.py
+++ examples/application_commands/message_commands.py
@@ -1,9 +1,6 @@
 import discord
 
 class MyClient(discord.Client):
-    def __init__(self):
-        super().__init__()
-
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
