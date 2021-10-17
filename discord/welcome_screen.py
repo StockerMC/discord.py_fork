1efe123212ba2956d6906dabca132d22d04144b8
StockerMC <44980366+StockerMC@users.noreply.github.com>
2021-10-17 12:40:09 -0400

    Refactor WelcomeScreenChannel.emoji
    Set WelcomeScreenChannel.emoji to an Emoji when applicable or a PartialEmoji with state

--- discord/welcome_screen.py
+++ discord/welcome_screen.py
@@ -26,21 +26,20 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Optional, List, TypeVar, Type, Union
 
-from .utils import MISSING
+from .utils import MISSING, _get_as_snowflake
 from .partial_emoji import PartialEmoji
 
 if TYPE_CHECKING:
     from .state import ConnectionState
     from .channel import TextChannel
     from .guild import Guild
-    from .invite import PartialInviteGuild
-    from .object import Object
 
     from .types.welcome_screen import (
         WelcomeScreen as WelcomeScreenPayload,
         WelcomeScreenChannel as WelcomeScreenChannelPayload,
         EditWelcomeScreen as EditWelcomeScreenPayload,
     )
+    from .types.emoji import Emoji as EmojiPayload
 
     WCT = TypeVar('WCT', bound='WelcomeScreenChannel')
 
@@ -63,7 +62,7 @@ class WelcomeScreen:
     enabled: :class:`bool`
         Whether the welcome screen is enabled. If this is ``False``,
         the welcome screen instance was edited with ``enabled=False``.
-    guild: Union[:class:`Guild`, :class:`PartialInviteGuild`, :class:`Object`]
+    guild: :class:`Guild`
         The guild that the welcome screen is for.
     """
 
@@ -75,27 +74,25 @@ class WelcomeScreen:
         '_state',
     )
 
-    def __init__(self, *, state: ConnectionState, data: WelcomeScreenPayload, guild: Union[Guild, PartialInviteGuild, Object]) -> None:
+    def __init__(self, *, state: ConnectionState, data: WelcomeScreenPayload, guild: Guild) -> None:
         self._state: ConnectionState = state
         self.enabled: bool = True
-        self.guild: Union[Guild, PartialInviteGuild, Object] = guild
-        self._update(data)
+        self.guild: Guild = guild
+        self.description: str = data['description']
+        welcome_channels = [WelcomeScreenChannel.from_data(parent=self, data=x) for x in data['welcome_channels']]
+        self.welcome_channels: List[WelcomeScreenChannel] = welcome_channels
 
     def __repr__(self) -> str:
         return f'<{self.__class__.__name__} enabled={self.enabled} description={self.description!r} welcome_channels={self.welcome_channels}>'
 
-    def _update(self, data: WelcomeScreenPayload) -> None:
-        self.description: str = data['description']
-        self.welcome_channels: List[WelcomeScreenChannel] = [WelcomeScreenChannel.from_data(state=self._state, data=x) for x in data['welcome_channels']]
-
     async def edit(
         self,
         *,
-        reason: Optional[str] = None,
         enabled: Optional[bool] = MISSING,
         welcome_channels: Optional[List[WelcomeScreenChannel]] = MISSING,
         description: Optional[str] = MISSING,
-    ) -> None:
+        reason: Optional[str] = None,
+    ) -> WelcomeScreen:
         """|coro|
 
         Edits the welcome screen in place.
@@ -144,7 +141,7 @@ class WelcomeScreen:
             ret['description'] = description
 
         data = await self._state.http.modify_guild_welcome_screen(self.guild.id, ret, reason=reason)
-        self._update(data)
+        return WelcomeScreen(state=self._state, data=data, guild=self.guild)
 
 
 class WelcomeScreenChannel:
@@ -156,8 +153,9 @@ class WelcomeScreenChannel:
         The channel's id
     description: :class:`str`
         The description shown for the channel.
-    emoji: Optional[:class:`PartialEmoji`]
-        The emoji for the channel, if one is set.
+    emoji: Optional[Union[:class:`Emoji`, :class:`PartialEmoji`]]
+        The emoji for the channel, if one is set. If the emoji is a
+        unicode emoji, it will be a :class:`PartialEmoji`.
     channel: Optional[:class:`TextChannel`]
         The channel for the welcome channel.
     """
@@ -166,17 +164,14 @@ class WelcomeScreenChannel:
         'channel_id',
         'description',
         'emoji',
-        '_state',
+        '_parent'
     )
 
-    def __init__(self, *, channel_id: int, description: str, emoji: Optional[PartialEmoji] = None) -> None:
+    def __init__(self, *, channel_id: int, description: str, emoji: Optional[Union[PartialEmoji, Emoji]] = None) -> None:
         self.channel_id: int = channel_id
-        self._state: Optional[ConnectionState] = None
         self.description: str = description
-
-        self.emoji: Optional[PartialEmoji] = emoji
-        if self.emoji is not None and self._state is not None:
-            self.emoji._state = self._state
+        self.emoji: Optional[Union[PartialEmoji, Emoji]] = emoji
+        self._parent: Optional[WelcomeScreen] = None
 
     def __repr__(self) -> str:
         return f'<{self.__class__.__name__} channel_id={self.channel_id} description={self.description!r} emoji={self.emoji!r}>'
@@ -185,15 +180,31 @@ class WelcomeScreenChannel:
     def channel(self) -> Optional[TextChannel]:
         """Optional[:class:`TextChannel`]: Returns the welcome screen channel's TextChannel object."""
         # the returned channel will either be a TextChannel or None
-        return self._state.get_channel(self.channel_id) if self._state is not None else None # type: ignore
+        state = self._parent and self._parent._state
+        return state and state.get_channel(self.channel_id)  # type: ignore
 
     @classmethod
-    def from_data(cls: Type[WCT], *, state: ConnectionState, data: WelcomeScreenChannelPayload) -> WCT:
-        channel_id = int(data.pop('channel_id'))
-        description = data.pop('description')
-        emoji = PartialEmoji.from_dict(data) if data else None # type: ignore
+    def from_data(cls: Type[WCT], *, parent: WelcomeScreen, data: WelcomeScreenChannelPayload) -> WCT:
+        channel_id = int(data['channel_id'])
+        description = data['description']
+        emoji_name = data['emoji_name']
+        emoji_id = _get_as_snowflake(data, 'emoji_id')
+        if emoji_id is not None:
+            # circular import
+            from .emoji import Emoji
+
+            emoji_data: EmojiPayload = {
+                'id': emoji_id,
+                'name': emoji_name,
+            }
+            emoji = Emoji(guild=parent.guild, state=parent._state, data=emoji_data)
+        elif emoji_name is not None:
+            emoji = PartialEmoji.with_state(state=parent._state, name=emoji_name)
+        else:
+            emoji = None
+
         self = cls(channel_id=channel_id, description=description, emoji=emoji)
-        self._state = state
+        self._parent = parent
         return self
 
     def to_dict(self) -> WelcomeScreenChannelPayload:
